a03772bc9b8a2b46e606fbb03edab26f16d2af93
Haf <haf@protonmail.ch>
2022-12-19 18:42:34 +0100

    cleaner error handling

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ use std::error::Error;
 use std::io::{Read, Write};
 use std::thread;
 
-fn printer_thread(mut printer_consumer: UnixStream) {
+fn printer_thread(mut printer_consumer: UnixStream) -> Result<(), Box<dyn Error>> {
     println!("Printer thread started");
     loop {
         let mut length_buffer = [0u8; 1];
-        printer_consumer.read_exact(&mut length_buffer).unwrap();
+        printer_consumer.read_exact(&mut length_buffer)?;
         
-        let length: usize = std::str::from_utf8(&length_buffer).unwrap().parse().unwrap();
+        let length: usize = std::str::from_utf8(&length_buffer)?.parse()?;
 
         let mut input_buffer = vec![0u8; length];
-        printer_consumer.read_exact(&mut input_buffer).unwrap();
+        printer_consumer.read_exact(&mut input_buffer)?;
         let actual_input = String::from_utf8_lossy(&input_buffer);
         println!("Printer received: {}", actual_input);
     }
@@ -22,7 +22,7 @@ fn printer_thread(mut printer_consumer: UnixStream) {
 fn main() -> Result<(), Box<dyn Error>> {
     let (mut printer_producer, printer_consumer) = UnixStream::pair()?;
 
-    let printer_handle = thread::spawn(|| { printer_thread(printer_consumer)});
+    let printer_handle = thread::spawn(|| { printer_thread(printer_consumer).unwrap() });
 
     let inputs = vec![
         "5first",
